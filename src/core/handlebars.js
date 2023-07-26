145a08a976c28f57b43a4082487729be719e5dc9
misbiheyv <misbiheyv@yandex-team.ru>
2023-07-26 17:35:45 +0300

    handlebars templates

---
+++ src/core/handlebars.js
@@ -0,0 +1,43 @@
+/* eslint-disable func-names */
+const {camelize, ucfirst} = require('./helpers');
+const Handlebars = require('handlebars');
+
+module.exports.getOutputFileInfo = getOutputFileInfo;
+
+Handlebars.registerHelper('camelize', function (args) {
+  return Handlebars.SafeString(camelize(args.fn(this)));
+});
+
+Handlebars.registerHelper('capitalize', function (args) {
+  return Handlebars.SafeString(ucfirst(camelize(args.fn(this))));
+});
+
+function getOutputFileInfo(path) {
+	const rl = readline.createInterface({
+		input: fs.createReadStream(path)
+	});
+
+	let
+    resolve,
+    reject;
+
+  const res = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+	rl.once('line', (line) => {
+    try {
+      const
+        {name} = /^{{!.*(name=(?<name>[A-Za-z.\-_]+?)[^A-Za-z.].*}}$)/g.exec(line).groups,
+        {ext} = /^{{!.*(extension|ext=(?<ext>[A-Za-z.]+?)[^A-Za-z.].*}}$)/g.exec(line).groups;
+
+      resolve({name, ext});
+
+    } catch (error) {
+      reject(error);
+    }
+	});
+
+  return res;
+}
--- src/templates/test/page/project/main.ts
+++
@@ -1,7 +0,0 @@
-import test from 'tests/config/project/test';
-
-test.describe('<p-name>', () => {
-	test('should be rendered', async ({ RName, page }) => {
-		await test.expect(page.locator('.p-name')).toBeVisible();
-	});
-});
